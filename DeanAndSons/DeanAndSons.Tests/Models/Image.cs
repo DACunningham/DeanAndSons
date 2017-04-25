c01d56db4edd18d94ba10bb7a7c9c3fdcf99fbdc
dexter <dextercunningham@hotmail.co.uk>
2017-04-25 15:39:52 +0100

    Added more unit test

---
+++ DeanAndSons/DeanAndSons.Tests/Models/Image.cs
@@ -0,0 +1,90 @@
+﻿//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using DeanAndSons.Models;
+//using System.Web;
+//using System.IO;
+//using System.Reflection;
+//using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+
+//namespace DeanAndSons.Tests.Models
+//{
+//    [TestClass]
+//    public class Image
+//    {
+//        [TestMethod]
+//        [HostType("ASP.NET")]
+//        [UrlToTest("http://localhost:27789/")]
+//        [AspNetDevelopmentServerHost("C:\\Users\\dexte\\Source\\Repos\\DeanAndSons\\DeanAndSons\\DeanAndSons", "C:\\Users\\dexte\\Source\\Repos\\DeanAndSons\\DeanAndSons\\DeanAndSons")]
+//        public void saveImageTest19()
+//        {
+//            // Arrange // Act
+//            // Create new objects with test post codes. This will invoke the getLatLong() method
+//            // where the external call to Google takes place
+//            //var constructorInfo = typeof(HttpPostedFile).GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance)[0];
+//            //var obj = (HttpPostedFile)constructorInfo
+//            //          .Invoke(new object[] { "filename", "image/jpeg", null });
+
+//            MemoryFile m = new MemoryFile();
+
+//            ImageProperty r1 = new ImageProperty(m, ImageType.PropertyHeader, "/Storage/Propertys", null);
+//            //Image r2 = new ImageProperty("", "", "", "", null, "", null);
+//            //Image r3 = new ImageProperty("", "", "", "123456789", null, "", null);
+//            //Image r4 = new ImageProperty("", "", "", "1234567890", null, "", null);
+
+//            // Assert
+//            //Assert.AreNotEqual<double>(0, r1.Lat);
+//            //Assert.AreNotEqual<double>(0, r1.Long);
+
+//            //Assert.AreEqual<double>(51.375814, r2.Lat);
+//            //Assert.AreEqual<double>(-2.359904, r2.Long);
+
+//            //Assert.AreEqual<double>(51.375814, r3.Lat);
+//            //Assert.AreEqual<double>(-2.359904, r3.Long);
+
+//            //Assert.AreEqual<double>(51.375814, r4.Lat);
+//            //Assert.AreEqual<double>(-2.359904, r4.Long);
+//        }
+//    }
+
+//    class MemoryFile : HttpPostedFileBase
+//    {
+//        Stream stream;
+//        string contentType;
+//        string fileName;
+
+//        public MemoryFile()
+//        {
+//            Stream s = File.Open("C:\\Users\\dexte\\Desktop\\Dining-Room.jpg", FileMode.Open);
+            
+//            this.stream = s;
+//            this.contentType = "image";
+//            this.fileName = "Dining-Room.jpg";
+//        }
+
+//        public override int ContentLength
+//        {
+//            get { return (int)stream.Length; }
+//        }
+
+//        public override string ContentType
+//        {
+//            get { return contentType; }
+//        }
+
+//        public override string FileName
+//        {
+//            get { return fileName; }
+//        }
+
+//        public override Stream InputStream
+//        {
+//            get { return stream; }
+//        }
+
+//        public override void SaveAs(string filename)
+//        {
+//            using (var file = File.Open(filename, FileMode.CreateNew))
+//                stream.CopyTo(file);
+//        }
+//    }
+//}
---
+++ DeanAndSons/DeanAndSons.Tests/Models/UnitTests.cs
@@ -0,0 +1,107 @@
+﻿using DeanAndSons.Models;
+using DeanAndSons.Models.IMS.ViewModels;
+using DeanAndSons.Models.WAP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace DeanAndSons.Tests.Models
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void IsCustomerTest19()
+        {
+            // Arrange
+            var r1 = new ApplicationUser();
+            var r2 = new Customer();
+            var r3 = new Staff();
+
+            // Act
+            var rb1 = ApplicationUser.IsCustomer(r1);
+            var rb2 = ApplicationUser.IsCustomer(r2);
+            var rb3 = ApplicationUser.IsCustomer(r3);
+            var rb4 = ApplicationUser.IsCustomer(null);
+
+
+            // Assert
+            Assert.AreEqual<bool>(false, rb1);
+            Assert.AreEqual<bool>(true, rb2);
+            Assert.AreEqual<bool>(false, rb3);
+            Assert.AreEqual<bool>(false, rb4);
+        }
+
+        [TestMethod]
+        public void addContactTest20()
+        {
+            // Arrange
+            var r1 = new ApplicationUser();
+            var r2 = new Customer();
+            var r3 = new Staff();
+            var _contact1 = new ContactUser("20", "test", "test", "test", 123, "test", r1);
+            var _contact2 = new ContactUser("20", "test", "test", "test", 123, "test", r2);
+            var _contact3 = new ContactUser("20", "test", "test", "test", 123, "test", r3);
+
+            // Act
+            r1.addContact("20", "test", "test", "test", 123, "test", r1);
+            r1.addContact(null, null, null,null,null,null,null);
+            r1.addContact("20", "test", "test", "test", 123, "test", null);
+            r1.addContact("20", "test", "test", "test", 123, "test", r1);
+
+            r2.addContact("20", "test", "test", "test", 123, "test", r2);
+            r2.addContact(null, null, null, null, null, null, null);
+            r2.addContact("20", "test", "test", "test", 123, "test", null);
+            r2.addContact("20", "test", "test", "test", 123, "test", r2);
+
+            r3.addContact("20", "test", "test", "test", 123, "test", r3);
+            r3.addContact(null, null, null, null, null, null, null);
+            r3.addContact("20", "test", "test", "test", 123, "test", null);
+            r3.addContact("20", "test", "test", "test", 123, "test", r3);
+
+
+            // Assert
+            Assert.AreEqual<string>(_contact1.PropertyNo, r1.Contact.Single().PropertyNo);
+            Assert.AreEqual<string>(_contact1.Town, r1.Contact.Single().Town);
+            Assert.AreEqual<ApplicationUser>(_contact1.ApplicationUser, r1.Contact.Single().ApplicationUser);
+
+            Assert.AreEqual<string>(_contact2.PropertyNo, r2.Contact.Single().PropertyNo);
+            Assert.AreEqual<string>(_contact2.Town, r2.Contact.Single().Town);
+            Assert.AreEqual<ApplicationUser>(_contact2.ApplicationUser, r2.Contact.Single().ApplicationUser);
+
+            Assert.AreEqual<string>(_contact3.PropertyNo, r3.Contact.Single().PropertyNo);
+            Assert.AreEqual<string>(_contact3.Town, r3.Contact.Single().Town);
+            Assert.AreEqual<ApplicationUser>(_contact3.ApplicationUser, r3.Contact.Single().ApplicationUser);
+        }
+
+        [TestMethod]
+        public void ServiceApplyEditIMSTest21()
+        {
+            // Arrange
+            var vm1 = new ServiceCreateIMSViewModel();
+            var vm2 = new ServiceEditIMSViewModel();
+            var vm3 = new ServiceEditIMSViewModel();
+
+            vm1.LastModified = DateTime.Now;
+            vm1.StaffOwnerID = "testFrom VM";
+
+            vm2.StaffOwnerID = "changed";
+            vm2.LastModified = DateTime.Now.AddDays(1);
+
+            vm3.StaffOwnerID = null;
+            vm3.LastModified = DateTime.Today;
+
+            var r1 = new Service(vm1);
+
+            // Act
+            r1.ApplyEditIMS(vm2);
+            r1.ApplyEditIMS(vm3);
+            r1.ApplyEditIMS(vm2);
+
+            // Assert
+            Assert.AreEqual<string>(vm2.StaffOwnerID, r1.StaffOwnerID);
+            Assert.AreEqual<DateTime>(vm2.LastModified, r1.LastModified);
+
+        }
+    }
+}
