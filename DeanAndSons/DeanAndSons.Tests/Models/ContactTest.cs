190d03d21a470f54b1f1981996142eb6878b8400
dexter <dextercunningham@hotmail.co.uk>
2017-04-25 11:37:52 +0100

    Added comments to contact tests

--- DeanAndSons/DeanAndSons.Tests/Models/ContactTest.cs
+++ DeanAndSons/DeanAndSons.Tests/Models/ContactTest.cs
@@ -5,19 +5,21 @@ using DeanAndSons.Models;
 namespace DeanAndSons.Tests.Models
 {
     [TestClass]
-    public class UnitTest1
+    public class ContactTest
     {
         [TestMethod]
-        public void GetLocationLatLongTest02()
+        public void GetLocationLatLongTest17()
         {
             // Arrange
 
             // Act
+            // Static method call to method using correct, incorrect and invalid input
             var r1 = Contact.GetLocationLatLng("Chippenham");
             var r2 = Contact.GetLocationLatLng(null);
             var r3 = Contact.GetLocationLatLng("nfuire;laugfd;sagfds56g43sdg879fr6ts43gr");
 
             // Assert
+            // Test if the Lat & Longs returned are correct given input
             Assert.AreEqual<double>(r1.Latitude, 51.461552);
             Assert.AreEqual<double>(r1.Longitude, -2.119497);
 
@@ -29,15 +31,14 @@ namespace DeanAndSons.Tests.Models
         }
 
         [TestMethod]
-        public void GetLatLongTest03()
+        public void GetLatLongTest18()
         {
             // Arrange // Act
+            // Create new objects with test post codes. This will invoke the getLatLong() method
+            // where the external call to Google takes place
             Contact r1 = new ContactProperty("", "", "", "WD19 5AX", null, "", null);
-
             Contact r2 = new ContactProperty("", "", "", "", null, "", null);
-
             Contact r3 = new ContactProperty("", "", "", "123456789", null, "", null);
-
             Contact r4 = new ContactProperty("", "", "", "1234567890", null, "", null);
 
             // Assert
